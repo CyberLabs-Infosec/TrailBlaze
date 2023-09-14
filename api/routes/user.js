ab3dad6df793504b48e2a6ab2d9dfdd1da59ed16
TheAlpha16 <101546002+TheAlpha16@users.noreply.github.com>
2023-09-14 23:13:19 +0530

    Update user.js
    - added adm_no field in edit

--- api/routes/user.js
+++ api/routes/user.js
@@ -17,16 +17,16 @@ router.route("/verify").get(async (req, res) => {
 })
 
 router.route("/edit").post(async (req, res) => {
-    const { username } = req.body;
+    const { username, adm_no } = req.body;
 
-    if (!username) {
+    if (!username || !adm_no) {
         return res.status(401).json({ status: "fail", error: "All fields are required" });
     }
 
     try {
-        await pool.query("UPDATE users SET username=$1 WHERE email=$2", [username, req.user.email]);
+        await pool.query("UPDATE users SET username=$1, adm_no=$2 WHERE email=$3", [username, adm_no, req.user.email]);
     } catch {
-        return res.status(401).json({ status: "fail", error: "The name already exists" });
+        return res.status(401).json({ status: "fail", error: "This should not happen, Contact Admin!" });
     }
     return res.status(200).json({ status: "success", error: "" });
 })
@@ -47,9 +47,9 @@ router.route("/createteam").post(async (req, res) => {
     try {
         const hashedSecret = await bcrypt.hash(secret, 10);
         
-        await pool.query("INSERT INTO teams (teamname, secret, team_scores, team_members, captain_id) VALUES ($1, $2, $3, $4, $5)", [teamname, hashedSecret, [[0, 0]], 1, req.user.id]);
+        await pool.query("INSERT INTO teams (teamname, secret, team_scores, team_members, captain_id) VALUES ($1, $2, $3, $4, $5)", [teamname, hashedSecret, [[0, 0]], 1, req.user.uid]);
         const qRes = await pool.query("SELECT team_id FROM teams WHERE teamname=$1", [teamname]);
-        await pool.query("UPDATE users SET team_id=$1 WHERE id=$2", [qRes.rows[0].team_id, req.user.id]);
+        await pool.query("UPDATE users SET team_id=$1 WHERE uid=$2", [qRes.rows[0].team_id, req.user.uid]);
 
         return res.status(200).json({ status: "success", error: "" });
     } catch(err) {
@@ -66,13 +66,13 @@ router.route("/jointeam").post(async (req, res) => {
     }
 
     try {
-        const usrTeam = await pool.query("SELECT team_id FROM users WHERE id=$1", [req.user.id]);
+        const usrTeam = await pool.query("SELECT team_id FROM users WHERE uid=$1", [req.user.uid]);
 
         if (usrTeam.rows[0].team_id == null) {
             const qRes = await pool.query("SELECT team_id, secret, team_members FROM teams WHERE teamname=$1", [teamname]);
 
             if (qRes.rowCount == 0) {
-                return res.status(400).json({ status: "fail", error: "Team does not exists" });
+                return res.status(400).json({ status: "fail", error: "Team does not exist" });
             }
 
             const match = await bcrypt.compare(secret, qRes.rows[0].secret);
@@ -81,7 +81,7 @@ router.route("/jointeam").post(async (req, res) => {
                 if (qRes.rows[0].team_members == 3) {
                     return res.status(400).json({ status: "fail", error: "Team is full :(" });
                 }
-                await pool.query("UPDATE users SET team_id=$1 WHERE id=$2", [qRes.rows[0].team_id, req.user.id]);
+                await pool.query("UPDATE users SET team_id=$1 WHERE uid=$2", [qRes.rows[0].team_id, req.user.uid]);
                 await pool.query("UPDATE teams SET team_members=$1", [qRes.rows[0].team_members + 1]);
             } else {
                 return res.status(400).json({ status: "fail", error: "Incorrect team credentials" });
@@ -101,12 +101,12 @@ router.route("/leaveteam").get(async (req, res) => {
     try {
         const qRes = await pool.query("SELECT captain_id, team_members FROM teams WHERE team_id=$1", [req.user.team_id]);
 
-        if (qRes.rows[0].captain_id == req.user.id) {
+        if (qRes.rows[0].captain_id == req.user.uid) {
             await pool.query("DELETE FROM teams WHERE team_id=$1", [req.user.team_id]);
-            return res.status(200).json({ status: "info", error: "", info: "The team was deleted since you were the last member" });
+            return res.status(200).json({ status: "info", error: "", info: "The team was deleted since you are the captain" });
         }
 
-        await pool.query("UPDATE users SET team_id=$1 WHERE id=$2", [null, req.user.id]);
+        await pool.query("UPDATE users SET team_id=$1 WHERE uid=$2", [null, req.user.uid]);
         await pool.query("UPDATE teams SET team_members=$1", [qRes.rows[0].team_members - 1]);
 
         return res.status(200).json({ status: "success", error: "" });
@@ -118,7 +118,7 @@ router.route("/leaveteam").get(async (req, res) => {
 
 router.route("/teaminfo").get(async (req, res) => {
     try {
-        const members = await pool.query("SELECT u.username, u.id FROM users AS u WHERE u.team_id=$1;", [req.user.team_id]);
+        const members = await pool.query("SELECT u.username, u.uid FROM users AS u WHERE u.team_id=$1;", [req.user.team_id]);
         const captain = await pool.query("SELECT t.captain_id FROM teams AS t WHERE t.team_id=$1", [req.user.team_id]);
         return res.status(200).json({ status: "success", error: "", data: members.rows, captain_id: captain.rows[0].captain_id });
     } catch(err) {
