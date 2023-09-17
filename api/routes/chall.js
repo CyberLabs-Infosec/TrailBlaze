f1c6cd763befed1d0fb41557bc2d8f07521a675d
Atish03 <atishpersonal003@gmail.com>
2023-09-17 10:47:46 +0530

    Added dummy challenges to db

---
+++ api/routes/chall.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const userMiddleware = require("../middleware/user");
+router.use(userMiddleware);
+
+const { pool } = require("../db");
+
+router.route("/getchalls").get(async (req, res) => {
+    const challs = await pool.query("SELECT chall_id, title, prompt, place, checkpoint, points, hints, solves, visible FROM challenges");
+    res.status(200).json({ status: "success", error: "", data: challs.rows, user: req.user });
+})
+
+module.exports = router;
--- client/app/challenges/page.tsx
+++ client/app/challenges/page.tsx
@@ -7,21 +7,20 @@ import { useRouter } from 'next/navigation'
 import Loading from '../../components/Loading';
 import Challenge from "../../components/Challenge";
 
-import challenges from "./challs.json";
-
 import { setNotification, getNotification } from '../../utils/notification';
 import { toast } from "react-toastify";
 
 export default function Page() {
     const [userdata, setUserData] = useState({ username: "" });
     const [loggedin, setLoggedin] = useState(false);
-    const [currChall, setCurrChall] = useState(challenges[0]);
+    const [currChall, setCurrChall] = useState(null);
+    const [challenges, setChallenges] = useState([]);
 
     const router = useRouter();
 
     const placeOnPath = (obj) => {
         var pos = obj.place / (challenges.length - 1);
-        var isCheckPoint = obj.checkPoint;
+        var isCheckPoint = obj.checkpoint;
 
         var trail = (document.getElementById("trail") as unknown as SVGGeometryElement);
         var pathLength = trail.getTotalLength();
@@ -44,35 +43,41 @@ export default function Page() {
     }
 
     useEffect(() => {
-        if (loggedin) {
+        if (loggedin && currChall != null) {
             for (var chall of challenges) {
                 placeOnPath(chall);
             }
         }
-    }, [loggedin])
+    }, [currChall])
 
     useEffect(() => {
-        const verify = async () => {
-            const data = await fetch("/api/user/verify", {
+        const getChalls = async () => {
+            const challs = await fetch("/api/chall/getchalls", {
                 headers: {
                     "Authorization": `Bearer ${Cookies.get('token')}`
                 }
             });
-            const jsonData = await data.json();
-            if (jsonData.status == "success") {
-                if (!jsonData.data.team_id) {
+            const challsToJson = await challs.json();
+            if (challsToJson.status == "success") {
+                if (!challsToJson.user.team_id) {
                     setNotification(true, "info", "Please join a team first!");
-                    router.push("/team");
+                    return router.push("/team");
                 } else {
-                    setUserData(jsonData.data);
+                    setUserData(challsToJson.user);
                     setLoggedin(true);
                 }
             } else {
-                router.push("/login")
+                setNotification(true, "error", "Please login");
+                return router.push("/");
             }
+            setChallenges(challsToJson.data);
         }
-        verify();
+        getChalls();
     }, [])
+
+    useEffect(() => {
+        setCurrChall(challenges[0]);
+    }, [challenges])
 
     useEffect(() => {
         const {toShow, type, message} = getNotification();
@@ -98,14 +103,17 @@ export default function Page() {
     }, [])
 
     return (
-        loggedin ? 
+        loggedin ? currChall != null ?
         <>
         <div className="absolute flex justify-center items-center w-full min-h-screen bg-slate-900 gap-10 h-fit" id="map">
             <Challenge chall={currChall}></Challenge>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350 350" id="container">
                 <path id="trail" d="m 54 29 c 87 12.6667 208 -9 261 38 c 28 27 23 62 -11 73 c -80 16 -172 -19 -250 15 c -33 16 -21 66 4 69 c 89 11 161 -33 257 14 c 29 17 27 25 21 48 c -7 27 -101.6667 13 -151 18" stroke="#32374d" strokeWidth="1" strokeLinecap="round" strokeDasharray="3,3" fill="none"/>
             </svg>
-        </div></>:
+        </div></>: 
+        <div className='text-center text-3xl font-bold text-slate-400'>
+            <Loading></Loading>
+        </div>:
         <div className='text-center text-3xl font-bold text-slate-400'>
             <Loading></Loading>
         </div>
