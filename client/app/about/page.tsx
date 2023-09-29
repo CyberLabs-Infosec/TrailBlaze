0d685f1f75b74278559d96327097537c7d029028
Atish03 <atishpersonal003@gmail.com>
2023-09-29 13:23:23 +0530

    Added invitation in about

--- client/app/about/page.tsx
+++ client/app/about/page.tsx
@@ -112,7 +112,7 @@ export default function() {
                     <div className="relative text-slate-400 w-min font-bold text-xl translate-y-32 flex flex-col gap-2 items-center"><p>SCROLL</p><span className="relative flex bg-downArrow bg-center bg-contain bg-no-repeat h-8 w-8"></span></div>
                 </div>
             </div>
-            <p id="heading" className="fixed top-32 opacity-0 w-full text-base font-bold text-slate-300 text-center z-20 scale-0">THE LOST ASTRONAUT</p>
+            <p id="heading" className="fixed top-32 opacity-0 w-full text-base font-black text-slate-300 text-center z-20 scale-0 mix-blend-difference">THE LOST ASTRONAUT</p>
             <div id="storyBoard" className="h-screen flex" style={{ width: "300vw" }}>
                 <div className="story h-full w-full bg-slate-900 flex justify-center items-center bg-center bg-no-repeat bg-cover" style={{ backgroundImage: "url('/static/assets/slide_1.jpg')" }}>
                     <div className="relative h-full w-full flex justify-center items-center" style={{ backgroundColor: "rgba(20, 20, 20, 0.75)" }}>
@@ -145,8 +145,28 @@ export default function() {
                 </div>
             </div>
             <div className="h-screen w-full bg-slate-900"></div>
-            <p id="howtoHeading" className="fixed top-32 opacity-0 w-full text-base font-bold text-slate-300 text-center z-20 scale-0">JOIN US AT WORKSHOP</p>
-            <div id="howTo" className="w-full h-screen bg-slate-900"></div>
+            <p id="howtoHeading" className="fixed top-32 opacity-0 w-full text-base font-bold text-slate-300 text-center z-20 scale-0 mix-blend-exclusion">JOIN US AT WORKSHOP</p>
+            <div id="howTo" className="w-full h-screen bg-slate-900 p-32 flex justify-center items-center">
+                <div className="relative top-4 h-full w-full rounded-lg bg-bottom bg-cover bg-no-repeat" style={{ backgroundImage: "url('https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2016/09/15/1331818231340_2/a-typical-scene-from-a-planet-in-no-man-s-sky-space-exploration-video-game')" }}>
+                    <div className="h-full w-full rounded-lg backdrop-blur-sm grid grid-cols-2 grid-rows-2 gap-8 p-8" style={{ backgroundColor: "rgba(50, 50, 50, 0.7)" }}>
+                        <div className="flex grid-cols-1 grid-rows-1 rounded-lg flex flex-col items-center p-4 gap-4">
+                            <p className="text-slate-300 font-bold text-5xl text-center">VENUE</p>
+                            <hr className="border-2 border-slate-400 w-full"></hr>
+                            <p className="font-bold text-2xl text-slate-300">TBD</p>
+                        </div>
+                        <div className="flex grid-cols-1 grid-rows-1 rounded-lg"></div>
+                        <div className="flex grid-cols-1 grid-rows-1 rounded-lg"></div>
+                        <div className="flex grid-cols-1 grid-rows-1 rounded-lg flex flex-col items-center p-4 gap-4">
+                            <p className="text-slate-300 font-bold text-5xl text-center">CONTACT</p>
+                            <hr className="border-2 border-slate-400 w-full"></hr>
+                            <div className="flex flex-col gap-3">
+                                <p className="font-bold text-2xl text-slate-300 tracking-wide">SUMIT GUPTA: 9309779260</p>
+                                <p className="font-bold text-2xl text-slate-300 tracking-wide">JIHENDRA NADH: 8688842828</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
