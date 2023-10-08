b2108d7f732142e7691cce72f64e5601ebe8fc7d
TheAlpha16 <101546002+TheAlpha16@users.noreply.github.com>
2023-10-08 16:29:47 +0530

    handled an error in getFlag

--- challenges/level_11/build/db.py
+++ challenges/level_11/build/db.py
@@ -12,7 +12,7 @@ def getFlag(team_id, place):
         return {"success": False, "data": f"chall_id doesn't exist for place: {place}"}
     chall_id = test[0]
     cursor.execute("SELECT flags->%s FROM teams WHERE team_id=%s", (str(chall_id), str(team_id)))
-    d = cursor.fetchone()["?column?"]
+    d = cursor.fetchone()[0]
     return {"success": True, "data": d}
 
 
