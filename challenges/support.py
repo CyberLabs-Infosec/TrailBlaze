ce1260d8fd56615d4e766e46b2525fff79ef9deb
TheAlpha16 <101546002+TheAlpha16@users.noreply.github.com>
2023-10-08 12:39:28 +0530

    added more challs

--- challenges/support.py
+++ challenges/support.py
@@ -1,11 +1,10 @@
 import psycopg2
 import psycopg2.extras
-import json
-import random
 from hashlib import md5
 import segno
 from PIL import Image
-import os
+
+import os, base64, random, json
 
 config = {
     "user": "postgres",
@@ -63,7 +62,25 @@ class buildFile:
                 return {"success": True, "data": f"tmp/{fileName}"}
             except Exception as e:
                 return {"success": False, "data": f"Error in level_15: QR generation {e}"}
-
+            
+        if str(place) == "2":
+            if file != "encoded.txt":
+                return {"success": False, "data": f"{file} not found place={place}"}
+            try:
+                open(f"tmp/{fileName}", "w").write(f"Here's the way forward: {base64.b64encode(flag.encode().hex().encode()).decode()}")
+                return {"success": True, "data": f"tmp/{fileName}"}
+            except Exception as e:
+                return {"success": False, "data": f"Error in level_2: encoding {e}"}
+        
+        if str(place) == "11":
+            if file != "repairMe.c":
+                return {"success": False, "data": f"{file} not found place={place}"}
+            try:
+                return {"success": True, "data": f"level_11/sources/{file}"}
+            except Exception as e:
+                return {"success": False, "data": f"Error in level_11: sharing {e}"}
+            
+            
 def getFlag(chall_id, team_id, place):
     curr = conn.cursor()
     try:
