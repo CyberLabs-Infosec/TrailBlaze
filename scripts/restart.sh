7a1b33aceb3be0781ec3758f4aecaba300e92cd8
TheAlpha16 <101546002+TheAlpha16@users.noreply.github.com>
2023-10-10 11:46:34 +0530

    added echo messages

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -3,16 +3,20 @@
 containers=(trailblaze-file_server-1 trailblaze-level_11-1 trailblaze-frontend-1 trailblaze-admin-1 trailblaze-backend-1 trailblaze-postgres-1)
 images=(trailblaze-level_11 trailblaze-file_server)
 
+echo "[*] removing old containers"
 for i in "${containers[@]}"
 do
     docker stop $i
     docker rm $i
 done
+echo "[+] containers removed successfully"
 
+echo "[*] removing images"
 for i in "${images[@]}"
 do
     docker rmi $i
 done
+echo "[+] images removed successfully"
 
 rm -rf ~/TrailBlaze/*/.next
 rm -rf ~/TrailBlaze/*/node_modules
