45ab647d15828e59c5d956d13c2a61b80a121203
Atish03 <atishpersonal003@gmail.com>
2023-10-11 23:40:44 +0530

    Fixed backup bug

--- db_backup/backups/script.sh
+++ db_backup/backups/script.sh
@@ -1,2 +1,2 @@
 #!/bin/ash
-su - postgres -c "pg_dump -h postgres -d postgres -U postgres > /var/lib/postgresql/backup.sql"
+pg_dump -h postgres -d postgres -U postgres > /var/lib/postgresql/backup.sql
