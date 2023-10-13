3708a6c607b0a5edc766ceba67a3b84828478a6e
TheAlpha16 <101546002+TheAlpha16@users.noreply.github.com>
2023-10-13 11:38:37 +0530

    updated prompts

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -4,7 +4,7 @@ containers=(trailblaze-file_server-1 trailblaze-frontend-1 trailblaze-admin-1 tr
 trailblaze-level_5-1 trailblaze-level_7-1 
 trailblaze-level_8-1 
 trailblaze-level_11-1 trailblaze-level_12-1 trailblaze-level_18-1 trailblaze-level_19-1 trailblaze-level_10-1 trailblaze-level_16-1)
-images=(trailblaze-level_11 trailblaze-file_server trailblaze-docker_bot trailblaze-fuel_cronjob trailblaze-level_7 trailblaze-level_12 trailblaze-level_19 trailblaze-level_10 trailblaze-level_16 trailblaze-level_18)
+images=(trailblaze-level_11 trailblaze-file_server trailblaze-docker_bot trailblaze-fuel_cronjob trailblaze-level_7 trailblaze-level_19 trailblaze-level_10 trailblaze-level_16 trailblaze-level_18)
 
 echo "[*] removing old containers (except database)"
 for i in "${containers[@]}"
