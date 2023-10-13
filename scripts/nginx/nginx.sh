d31cabc2d85e53aa9165860db18bb8b15b4dd955
TheAlpha16 <101546002+TheAlpha16@users.noreply.github.com>
2023-10-13 10:59:04 +0530

    changed checksum subdomain

--- scripts/nginx/nginx.sh
+++ scripts/nginx/nginx.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo cp ~/TrailBlaze/scripts/nginx/trailblaze.conf ~/TrailBlaze/scripts/nginx/admin.conf ~/TrailBlaze/scripts/nginx/inspect.conf ~/TrailBlaze/scripts/nginx/checkforsum.conf ~/TrailBlaze/scripts/nginx/revjs.conf ~/TrailBlaze/scripts/nginx/maxxor.conf ~/TrailBlaze/scripts/nginx/overflow.conf ~/TrailBlaze/scripts/nginx/readme.conf ~/TrailBlaze/scripts/nginx/cookie.conf ~/TrailBlaze/scripts/nginx/jwt.conf ~/TrailBlaze/scripts/nginx/bluff.conf /etc/nginx/sites-available
+sudo cp ~/TrailBlaze/scripts/nginx/trailblaze.conf ~/TrailBlaze/scripts/nginx/admin.conf ~/TrailBlaze/scripts/nginx/inspect.conf ~/TrailBlaze/scripts/nginx/checksum.conf ~/TrailBlaze/scripts/nginx/revjs.conf ~/TrailBlaze/scripts/nginx/maxxor.conf ~/TrailBlaze/scripts/nginx/overflow.conf ~/TrailBlaze/scripts/nginx/readme.conf ~/TrailBlaze/scripts/nginx/cookie.conf ~/TrailBlaze/scripts/nginx/jwt.conf ~/TrailBlaze/scripts/nginx/bluff.conf /etc/nginx/sites-available
 echo "[+] copied config files successfully to /etc/nginx/sites-available"
 
 sudo ln -s /etc/nginx/sites-available/trailblaze.conf /etc/nginx/sites-enabled
 sudo ln -s /etc/nginx/sites-available/inspect.conf /etc/nginx/sites-enabled
-sudo ln -s /etc/nginx/sites-available/checkforsum.conf /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/checksum.conf /etc/nginx/sites-enabled
 sudo ln -s /etc/nginx/sites-available/revjs.conf /etc/nginx/sites-enabled
 sudo ln -s /etc/nginx/sites-available/maxxor.conf /etc/nginx/sites-enabled
 sudo ln -s /etc/nginx/sites-available/overflow.conf /etc/nginx/sites-enabled
